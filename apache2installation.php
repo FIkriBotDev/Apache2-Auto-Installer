41d26dbf8ff8458b7b0074702e58162ac3581b29
FIkriBotDev <132673557+FIkriBotDev@users.noreply.github.com>
2023-09-01 10:50:29 +0800

    Update apache2installation.php

--- apache2installation.php
+++ apache2installation.php
@@ -1,1 +1,7 @@
-<?php echo "Apache2 Setup Successfully! \n"; ?>
+<?php
+echo "Apache2 Setup Successfully! \n";
+echo "You can open your site on: http://yoursiteip.domain:port/ \n";
+echo "If you port is default(80) open your site on: http://yoursiteip.domain/ \n";
+echo "\n";
+echo "You can edit apache2 port on the /etc/apache2/ports.conf using nano or vim \n";
+?>
