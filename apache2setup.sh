7e9e06370ac84d1e4dc27eb142ddee05486d3f34
FIkriBotDev <132673557+FIkriBotDev@users.noreply.github.com>
2023-09-01 13:17:36 +0800

    Update apache2setup.sh

--- apache2setup.sh
+++ apache2setup.sh
@@ -4,6 +4,8 @@ rm /etc/apache2/ports.conf
 cp ports.conf /etc/apache2/
 /etc/init.d/apache2 restart
 sudo apt install php -y
+cp index.php /var/www/html/
+rm /var/www/html/index.html
 sudo apt install neofetch -y
 neofetch
 php apache2installation.php
