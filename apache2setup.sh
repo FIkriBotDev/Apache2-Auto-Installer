388d168cac15bbb2c5217cc678863c339864b974
FIkriBotDev <132673557+FIkriBotDev@users.noreply.github.com>
2023-09-01 10:46:39 +0800

    Update apache2setup.sh

--- apache2setup.sh
+++ apache2setup.sh
@@ -1,7 +1,7 @@
 sudo apt update -y
 sudo apt install apache2 -y
 rm /etc/apache2/ports.conf
-mv /home/kacungnetwork/ports.conf /etc/apache2/
+cp ports.conf /etc/apache2/
 /etc/init.d/apache2 restart
 sudo apt install php -y
 php apache2installation.php
