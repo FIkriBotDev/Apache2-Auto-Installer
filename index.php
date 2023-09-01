4051bc5b8c87d4915e5c98cc2b4644451e4d76f0
FIkriBotDev <132673557+FIkriBotDev@users.noreply.github.com>
2023-09-01 13:20:57 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apache2 Successfully Installed!</title>
+    <meta name="description" content="Powered by Apache2 Auto Installer">
+</head>
+<body>
+    <center>
+        <h1>Apache2 Successfully Installed</h1>
+        <br />
+        <br />
+        <h3>Thanks TO:<br />
+            F1KR7D3V
+        </h3>
+    </center>
+</body>
+</html>
